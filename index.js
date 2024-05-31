dafff8d932c6c26d662cd30c192e12d753312f28
Rijvi Islam <rijviislam54@gmail.com>
2024-06-01 03:58:17 +0600

    feat: added user post method

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // MIDDLE-WARE //
 app.use(cors());
 app.use(express.json());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_TECHSPOTTER_USER}:${process.env.DB_TECHSPOTTER_PASS}@cluster0.yy4jwyq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,28 +17,41 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+    const usersCollection = client.db("techSpotterDB").collection("users");
+
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const isExistUser = await usersCollection.findOne(query);
+      if (isExistUser) {
+        return res.send({ message: "user already exist!", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-app.get('/',(req, res) => {
-    res.send('Tech Spotter is running!!!')
-})
+app.get("/", (req, res) => {
+  res.send("Tech Spotter is running!!!");
+});
 app.listen(port, () => {
-    console.log(`Tech Spotter is running on Port ${port} `)
-})
+  console.log(`Tech Spotter is running on Port ${port} `);
+});
